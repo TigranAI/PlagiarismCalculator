1fea43adea51b1a0161e2baa256fe795b4c86af2
Tigran Shaginyan <68780335+TigranAI@users.noreply.github.com>
2023-09-30 12:30:19 +0300

    Переписал под локальный запуск

--- src/main/java/ru/tigran/PlagiarismCalculator/PlagiarismCalculatorService.java
+++ src/main/java/ru/tigran/PlagiarismCalculator/PlagiarismCalculatorService.java
@@ -2,12 +2,26 @@ package ru.tigran.PlagiarismCalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class PlagiarismCalculatorService {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(PlagiarismCalculatorService.class, args);
+
+        Map<String, String> argsParsed = new HashMap<>();
+        for (String arg : args) {
+            if (arg.contains("=")) {
+                String[] argSplitted = arg.split("=");
+                argsParsed.put(argSplitted[0], argSplitted[1]);
+            }
+        }
+        File file1 = new File(argsParsed.get("--file1"));
+        File file2 = new File(argsParsed.get("--file2"));
+        CPP14AnalyzeWorker.fromFiles(file1, file2).run();
     }
 }
